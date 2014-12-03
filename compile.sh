209f9e441288d28d15fb42390f284db7be9a472e
Miro Nieminen <miro.nieminen@futurice.com>
2014-12-03 15:00:35 +0200

    Emergency alert defcon 5 commit

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 cd /Users/mnie/projects/thesis/
 rm -f thesis-mnie.pdf
 rm -f thesis-mnie.aux
-pdflatex -interaction=nonstopmode thesis-mnie.tex 2>> ../errorlog && bibtex thesis-mnie.aux 2>> ../errorlog && pdflatex -interaction=nonstopmode thesis-mnie.tex 2>> ../errorlog && pdflatex -interaction=nonstopmode thesis-mnie.tex 2>> ../errorlog && echo "Compiled by hand"
+pdflatex -interaction=nonstopmode thesis-mnie.tex 2>> thesis-mnie-errorlog.log && bibtex thesis-mnie.aux 2>> thesis-mnie-errorlog.log && pdflatex -interaction=nonstopmode thesis-mnie.tex 2>> thesis-mnie-errorlog.log && pdflatex -interaction=nonstopmode thesis-mnie.tex 2>> thesis-mnie-errorlog.log && echo "Compiled by hand"
 date >> ../log
 echo "Compiled by hand" >> ../log
